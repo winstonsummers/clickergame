4604fbe828ade0961b0f40c7f4ea7369481b3d9c
Winston Summers <winstonsummers@WinstonmatoMacBook-Pro.local>
2018-05-07 10:01:31 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="money"></div>
+        <div className="workers"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
